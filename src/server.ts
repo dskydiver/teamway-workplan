a8e80a139ba8078b55f837faf800da8e278ee466
dskydiver <dastanmukhan6@gmail.com>
2024-03-25 13:45:54 -0500

    separate server declear and start

---
+++ src/server.ts
@@ -0,0 +1,97 @@
+import { fastifySwagger, SwaggerOptions } from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import dotenv from 'dotenv'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod2'
+import shift from './routes/shift'
+import worker from './routes/worker'
+import logger from '../logger'
+
+export const server = fastify().withTypeProvider<ZodTypeProvider>()
+
+export const start = async () => {
+  try {
+    dotenv.config()
+
+    server.setValidatorCompiler(validatorCompiler)
+    server.setSerializerCompiler(serializerCompiler)
+
+    const swaggerOptions: SwaggerOptions = {
+      swagger: {
+        info: {
+          title: 'API Documentation',
+          description: 'API documentation for your Fastify application.',
+          version: '1.0.0',
+        },
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        securityDefinitions: {
+          BearerAuth: {
+            type: 'apiKey',
+            name: 'Authorization',
+            in: 'header',
+          },
+        },
+      },
+      transform: jsonSchemaTransform,
+    }
+
+    const swaggerUiOptions = {
+      routePrefix: 'documentation',
+      exposeRoute: true,
+    }
+
+    // Register the Swagger plugin
+    server.register(fastifySwagger, swaggerOptions)
+
+    server.register(fastifySwaggerUI, swaggerUiOptions)
+
+    server.register(worker)
+    server.register(shift)
+
+    server.get('/', (req, reply) => {
+      reply.send({ success: true, message: 'Welcome to the API' })
+    })
+
+    // if route not found
+    server.setNotFoundHandler((request, reply) => {
+      reply.code(404).send({ success: false, message: 'Route not found' })
+    })
+
+    if (process.env.DEPLOY_ENV === 'production') {
+      server.setErrorHandler((error, request, reply) => {
+        logger.debug(error)
+        if (error.code === 'FST_ERR_VALIDATION') {
+          reply.status(400).send({
+            error: 'Bad Request',
+            message: 'Invalid request parameters',
+          })
+        } else {
+          reply.send(error)
+        }
+      })
+    }
+
+    process.on('unhandledRejection', function (reason: any, promise: any) {
+      console.log(
+        'unhandledRejection ' +
+          promise.toString() +
+          ' stack ' +
+          JSON.stringify(reason.stack)
+      )
+    })
+
+    const portDetail: number = Number(process.env.PORT) || 5000
+
+    await server.listen({ port: portDetail })
+    logger.info(`server running on port ${portDetail}`)
+  } catch (error) {
+    server.log.error(error)
+    process.exit(1)
+  }
+}
